9b441d893d0d65c45ca38851a5517464cfcf5027
rejigo406 <rejigo406@gmail.com>
2025-07-22 01:25:41 -0700

    Update mailRoutes.js

--- routes/mailRoutes.js
+++ routes/mailRoutes.js
@@ -5,3 +5,11 @@ const { getMails } = require('../controllers/mailController');
 router.get('/mails', getMails);
 
 module.exports = router;
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('📬 QuickMail API is working!');
+});
+
+module.exports = router;
