c0999e2ddac26abb71b235fb0c2227ffd3f4c543
rejigo406 <rejigo406@gmail.com>
2025-07-22 01:02:46 -0700

    Delete mailRoutes.js

--- mailRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMails } = require('../controllers/mailController');
-
-router.get('/mails', getMails);
-
-module.exports = router;
